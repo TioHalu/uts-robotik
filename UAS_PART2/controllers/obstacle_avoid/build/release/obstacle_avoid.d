dfb2ff542d2952eb19701a951384660b6e6843e9
Agustio <agustio719@gmail.com>
2023-01-20 14:45:53 +0700

    Tutorial 6: 4-Wheels Robot

---
+++ UAS_PART2/controllers/obstacle_avoid/build/release/obstacle_avoid.d
@@ -0,0 +1,6 @@
+build/release/obstacle_avoid.o: obstacle_avoid.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
